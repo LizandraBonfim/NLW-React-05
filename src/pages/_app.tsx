8486f9ad661bec64a99ecba2525d61fad6e669f9
Lizandra Bonfim <lizandra@buildbox.com.br>
2021-04-20 20:43:58 -0300

    Fix: Wrapper App

---
+++ src/pages/_app.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/global.scss";
+import styles from "../styles/app.module.scss";
+import { Player } from "../components/Player";
+import { Header } from "../components/Header";
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
+    </div>
+  );
+}
+
+export default MyApp;
